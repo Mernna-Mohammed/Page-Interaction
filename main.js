31d79b20aeab9df1ae9bc49e7811b396fae93133
Mernna-Mohammed <mernna56@gmail.com>
2025-09-04 01:07:15 +0300

    pages

---
+++ main.js
@@ -0,0 +1,9 @@
+function color(btn) {
+    btn.style.backgroundColor = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`
+}
+
+function area() {
+    alert(`${window.innerHeight} x ${window.innerWidth}`)
+}
+
+
